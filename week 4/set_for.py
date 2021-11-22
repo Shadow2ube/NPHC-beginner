95dec1890135c580d8455ce6d85b1fe51d67ecb0
Christian <christianlaforest45@gmail.com>
2021-11-22 16:53:01 -0500

    ignore

--- week 4/set_for.py
+++ week 4/set_for.py
@@ -3,5 +3,8 @@ set = {1, "23", 4.5687}
 
 # will be random order each time
 
-for i in range(len(set)):
-    print(set[i])
+# for i in range(0, len(set)):
+#     print(set[i])
+
+for i in set:
+    print(i)
--- week 4/tuple.py
+++ week 4/tuple.py
@@ -3,3 +3,9 @@
 tuple = (1, "23", 4.5687)
 
 print(tuple)
+
+# can have duplicates
+
+tuple = (1, "23", 4.5687, "23")
+
+print(tuple)
